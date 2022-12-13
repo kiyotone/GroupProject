cd17379a64c48a36d2bc0b3caa6f2ad5bed2461c
Prateek61 <prateekpoudel61@gmail.com>
2022-12-13 17:10:29 +0545

    completed register and login page

--- frontend/pages/auth/Login.js
+++ frontend/pages/auth/Login.js
@@ -1,9 +1,23 @@
 import React from 'react'
-import styles from './auth.module.scss'
 
 function Login() {
   return (
-    <div className={`${styles.hola} text-center`}>Login</div>
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <form className='w-96 p-6 shadow-lg rounded-md bg-white'>
+        <h1 className='text-3xl block text-center font-semibold'>
+          Login
+        </h1>
+        <hr className='mt-3'></hr>
+        <div className='mt-3'>
+          <label for='username' className='block text-base mb-2'>Username</label>
+          <input type='text' id='username' className='border w-full text-base px-2 py-1 focus:outline-none focus:rind-0 focus:border-gray-600 bg-white' placeholder='Enter Username'></input>
+        </div>
+        <div className='mt-3'>
+          <label for='password' className='block text-base mb-2'>Password</label>
+          <input type='password' id='password' className='border w-full text-base px-2 py-1 focus:outline-none focus:rind-0 focus:border-gray-600 bg-white' placeholder='Enter Password'></input>
+        </div>
+      </form>
+    </div>
   )
 }
 
--- frontend/pages/auth/Register.js
+++ frontend/pages/auth/Register.js
@@ -1,14 +1,25 @@
 import React from 'react'
-import s from './auth.module.scss'
 
 function Regster() {
   return (
-    <div className="w-full h-full">
-      <form className={s.auth_form}>
-        <label className={s.inp_bar_label}>
-          Enter your name
-          <input type='text'></input>
-        </label>
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <form className='w-96 p-6 shadow-lg rounded-md bg-white'>
+        <h1 className='text-3xl block text-center font-semibold'>
+          Register
+        </h1>
+        <hr className='mt-3'></hr>
+        <div className='mt-3'>
+          <label for='username' className='block text-base mb-2'>Username</label>
+          <input type='text' id='username' className='border w-full text-base px-2 py-1 focus:outline-none focus:rind-0 focus:border-gray-600 bg-white' placeholder='Enter Username'></input>
+        </div>
+        <div className='mt-3'>
+          <label for='password' className='block text-base mb-2'>Password</label>
+          <input type='password' id='password' className='border w-full text-base px-2 py-1 focus:outline-none focus:rind-0 focus:border-gray-600 bg-white' placeholder='Enter Password'></input>
+        </div>
+        <div className='mt-3'>
+          <label for='passwordConfirmation' className='block text-base mb-2'>Password Confirmation</label>
+          <input type='password' id='passwordConfirmation' className='border w-full text-base px-2 py-1 focus:outline-none focus:rind-0 focus:border-gray-600 bg-white' placeholder='Enter Password Again'></input>
+        </div>
       </form>
     </div>
   )
