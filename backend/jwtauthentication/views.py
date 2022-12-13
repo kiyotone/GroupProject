6b048906c63dad361db80e9d33144c19e8089d3a
Prateek61 <prateekpoudel61@gmail.com>
2022-12-13 23:20:45 +0545

    added endpoints for user authentication

--- backend/jwtauthentication/views.py
+++ backend/jwtauthentication/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.request import Request
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 # Create your views here.
+
+class RegisterApi(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def post(self, request: Request) -> Response:
+        data = request.data
+        username = data['username']
+        password = data['password']
+
+        # Attempt to create a new user
+        try:
+            user = User.objects.create_user(username=username, password=password)
+            user.save()
+        except IntegrityError:
+           return Response({'detail': 'User already exists'}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response({'detail': 'User created successfully'}, status=status.HTTP_201_CREATED)
