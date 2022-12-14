dc94a41d9ec36be327b820cdbf7fd5984c49e0cb
Prateek61 <prateekpoudel61@gmail.com>
2022-12-14 23:27:18 +0545

    Added endpoint for auth/getuser

--- backend/jwtauthentication/urls.py
+++ backend/jwtauthentication/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
-from .views import RegisterApi
+from .views import RegisterApi, GetUserApi
 
 urlpatterns = [
     path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify', TokenVerifyView.as_view(), name='token_verify'),
-    path('register', RegisterApi.as_view(), name='register')
+    path('register', RegisterApi.as_view(), name='register'),
+    path('getuser', GetUserApi.as_view(), name='get_user')
 ]
