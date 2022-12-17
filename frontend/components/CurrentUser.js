848ee7c2b4d4539546940e94c560d5007386dc5c
kiyotone <kunwarkirtan36@gmail.com>
2022-12-17 20:17:02 +0545

    new files

---
+++ frontend/components/CurrentUser.js
@@ -0,0 +1,32 @@
+import React, { createContext, useContext, useState } from 'react'
+
+const userContext = createContext()
+
+export function ProvideCurrentUser({children}) {
+    const state = useProvideCurrentUser();
+
+    return ( 
+       <userContext.Provider value={state}>{children}</userContext.Provider>
+    )
+
+
+}
+
+export function useCurrentUser(){
+    return useContext(userContext);
+}
+
+function useProvideCurrentUser(){
+    const [USERNAME,setUser] = useState("")
+    
+    const CHANGE_USER = (e) =>{
+        setUser(e);
+    }
+
+    return {
+        USERNAME,
+        CHANGE_USER
+    }
+
+}
+
